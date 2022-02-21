53d11323b833b8aaafebd184a1faac770729ea42
pechkin <pechkindev@gmail.com>
2022-02-21 21:39:21 +0400

    Deploy fixes

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -8,16 +8,16 @@ interface Props {
 }
 
 const User : FC<Props> = ({userId}) => {
-  const users = useAppSelector((state) => state.users)
-  const currentUserId = useAppSelector((state) => state.currentUserId)
-  const user = users[userId]
-  const currentUser = users[currentUserId]
-  const isCurrentUserProfile = userId !== currentUserId
+  // const users = useAppSelector((state) => state.users)
+  // const currentUserId = useAppSelector((state) => state.currentUserId)
+  // const user = users[userId]
+  // const currentUser = users[currentUserId]
+  // const isCurrentUserProfile = userId !== currentUserId
 
   return (
     <>
-      <div>User id {user.id}</div>
-      <div>User email {user.email}</div>
+      {/* <div>User id {user.id}</div>
+      <div>User email {user.email}</div> */}
     </>
   )
 }
